81d828ab09fc4f22b87055ea6c3182358625976c
Adraas <pickalov.artyom@yandex.ru>
2019-10-03 18:29:01 +0400

    feat: add object-relational mapping for entities

--- repository/src/main/java/ru/wkn/entities/HtmlTag.java
+++ repository/src/main/java/ru/wkn/entities/HtmlTag.java
@@ -5,6 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapKeyColumn;
 import java.util.Map;
 
 /**
@@ -12,6 +20,7 @@ import java.util.Map;
  *
  * @author Orin Adraas
  */
+@Entity(name = "html_tag")
 @NoArgsConstructor
 @EqualsAndHashCode
 @ToString
@@ -21,16 +30,24 @@ public class HtmlTag {
     /**
      * The ID of the represented HTML tag.
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     /**
      * The HTML attributes of the HTML tag.
      */
+    @MapKeyColumn(name = "attribute", unique = true, length = 30)
+    @Column(name = "attribute_value", length = 30)
+    @ElementCollection(targetClass = String.class, fetch = FetchType.EAGER)
     private Map<String, String> htmlAttributes;
 
     /**
      * The stylesheet (CSS) for the HTML tag.
      */
+    @MapKeyColumn(name = "style_parameter", unique = true, length = 60)
+    @Column(name = "style_parameter_value", length = 60)
+    @ElementCollection(targetClass = String.class, fetch = FetchType.EAGER)
     private Map<String, String> stylesheet;
 
     /**
--- repository/src/main/java/ru/wkn/entities/UserInputEntry.java
+++ repository/src/main/java/ru/wkn/entities/UserInputEntry.java
@@ -5,12 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * The {@code UserInputEntry} class represents the data entered by the user as a single {@code String} line with
  * an indicated delimiter.
  *
  * @author Orin Adraas
  */
+@Entity(name = "user_input_entry")
 @NoArgsConstructor
 @EqualsAndHashCode
 @ToString
@@ -20,16 +29,21 @@ public class UserInputEntry {
     /**
      * The ID of the represented user data.
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     /**
      * The delimiter of the data entered by the user.
      */
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "delimiter", nullable = false, length = 2)
     private UserInputEntryDelimiter userInputEntryDelimiter;
 
     /**
      * The data entered by the user as a single {@code String} line.
      */
+    @Column(name = "value")
     private String inputEntry;
 
     /**
