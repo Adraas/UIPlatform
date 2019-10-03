ad9091e3ec0d93c573b1a896dff5fd4aba1d75fa
Adraas <pickalov.artyom@yandex.ru>
2019-10-03 20:54:30 +0400

    docs: add documentation for the service interface

--- repository/src/main/java/ru/wkn/service/IService.java
+++ repository/src/main/java/ru/wkn/service/IService.java
@@ -2,7 +2,19 @@ package ru.wkn.service;
 
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * The interface {@code IService} represents a repository service layer.
+ *
+ * @param <K> the type of the ID of the entity the repository manages
+ * @param <V> the domain type the repository manages
+ * @author Orin Adraas
+ */
 public interface IService<K, V> {
 
+    /**
+     * The method for the {@code CrudRepository} instance obtaining.
+     *
+     * @return the {@code CrudRepository} object
+     */
     CrudRepository<V, K> getRepository();
 }
