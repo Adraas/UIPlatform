9316de8a068ad89dc8c1cbe8fa7fd47d973ce80c
Adraas <pickalov.artyom@yandex.ru>
2019-10-03 19:43:56 +0400

    docs: add documentation for the enum-entity

--- repository/src/main/java/ru/wkn/entities/HtmlTagType.java
+++ repository/src/main/java/ru/wkn/entities/HtmlTagType.java
@@ -5,15 +5,38 @@ import lombok.Getter;
 
 import javax.persistence.Column;
 
+/**
+ * The {@code HtmlTafType} enum represents a HTML tag type.
+ *
+ * @author Orin Adraas
+ */
 @AllArgsConstructor
 @Getter
 public enum HtmlTagType {
 
+    /**
+     * The {@code input} HTML-element representation.
+     */
     INPUT("input"),
+
+    /**
+     * The {@code select} HTML-element representation.
+     */
     SELECT("select"),
+
+    /**
+     * The {@code checkbox} HTML-element representation.
+     */
     CHECKBOX("checkbox"),
+
+    /**
+     * The {@code textarea} HTML-element representation.
+     */
     TEXTAREA("textarea");
 
+    /**
+     * The HTML tag type as a {@code String} object.
+     */
     @Column(name = "type", nullable = false, length = 20)
     private String type;
 }
