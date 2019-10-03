b83fc5b2efa59f5bfa625499d6a2b2b1ce57057c
Adraas <pickalov.artyom@yandex.ru>
2019-10-03 20:26:11 +0400

    docs: add documentation for the repository interfaces

--- repository/src/main/java/ru/wkn/repository/HtmlTagRepository.java
+++ repository/src/main/java/ru/wkn/repository/HtmlTagRepository.java
@@ -4,7 +4,22 @@ import org.springframework.data.repository.CrudRepository;
 import ru.wkn.entities.HtmlTag;
 import ru.wkn.entities.HtmlTagType;
 
+import java.util.Collection;
+
+/**
+ * The interface {@code HtmlTagRepository} represents an abstract repository with the specific Spring Data JPA style.
+ * This interface provides customized functional for a work with a datasource of {@code HtmlTag} type entities.
+ * It's the DAO layer of the repository.
+ *
+ * @author Orin Adraas
+ */
 public interface HtmlTagRepository extends CrudRepository<HtmlTag, Integer> {
 
-    HtmlTag getHtmlTagByType(HtmlTagType htmlTagType);
+    /**
+     * The method for the finding HTML tags ({@code HtmlTag} type) by the given HTML tag type ({@code HtmlTagType} type).
+     *
+     * @param htmlTagType the given HTML tag type for the searching
+     * @return a collection of found {@code HtmlTag} objects
+     */
+    Collection<HtmlTag> findHtmlTagsByType(HtmlTagType htmlTagType);
 }
--- repository/src/main/java/ru/wkn/repository/UserInputEntryRepository.java
+++ repository/src/main/java/ru/wkn/repository/UserInputEntryRepository.java
@@ -3,7 +3,21 @@ package ru.wkn.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.wkn.entities.UserInputEntry;
 
+/**
+ * The interface {@code UserInputEntryRepository} represents an abstract repository with the specific Spring Data JPA
+ * style. This interface provides customized functional for a work with a datasource of {@code UserInputEntry} type
+ * entities. It's the DAO layer of the repository.
+ *
+ * @author Orin Adraas
+ */
 public interface UserInputEntryRepository extends CrudRepository<UserInputEntry, Integer> {
 
+    /**
+     * The method for the obtaining user input entries ({@code UserInputEntry} type) by the given input entry as a
+     * {@code String} value.
+     *
+     * @param inputEntry the given input entry as a {@code String} value for the searching
+     * @return a found {@code UserInputEntry} object
+     */
     UserInputEntry getUserInputEntryByInputEntry(String inputEntry);
 }
