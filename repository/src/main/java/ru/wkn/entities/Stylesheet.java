e182d6fa1a533ceac7bc4c1ff45e5f6b2efd6b5a
Adraas <pickalov.artyom@yandex.ru>
2019-10-04 18:59:52 +0400

    docs: add documentation for the CSS entity

--- repository/src/main/java/ru/wkn/entities/Stylesheet.java
+++ repository/src/main/java/ru/wkn/entities/Stylesheet.java
@@ -5,14 +5,36 @@ import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
+/**
+ * The {@code Stylesheet} class represents the CSS for the repository.
+ *
+ * @author Orin Adraas
+ */
 @NoArgsConstructor
 @Getter
 public class Stylesheet {
 
+    /**
+     * The ID of the represented CSS.
+     */
     private int id;
+
+    /**
+     * The selector of the represented CSS.
+     */
     private String selector;
+
+    /**
+     * The declaration of the represented CSS.
+     */
     private Map<String, String> declaration;
 
+    /**
+     * Initializes a newly created {@code HtmlTag} object with a simple assignment of parameters to the fields.
+     *
+     * @param selector    {@link #selector}
+     * @param declaration {@link #declaration}
+     */
     public Stylesheet(String selector, Map<String, String> declaration) {
         this.selector = selector;
         this.declaration = declaration;
