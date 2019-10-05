40e195043ea24603f036a092cd5e6f10d2bf8ded
Adraas <pickalov.artyom@yandex.ru>
2019-10-05 14:42:31 +0400

    feat: add the facade pattern implementation

---
+++ repository/src/main/java/ru/wkn/RepositoryFacade.java
@@ -0,0 +1,32 @@
+package ru.wkn;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.wkn.service.IService;
+
+/**
+ * The class {@code RepositoryFacade} represents the facade pattern of the repository.
+ *
+ * @author Orin Adraas
+ */
+@Getter
+@Setter
+public class RepositoryFacade {
+
+    /**
+     * The abstract representation of the repository service.
+     */
+    private IService service;
+
+    /**
+     * Initializes a newly created {@code RepositoryFacade} object with the assignment value from the context for the
+     * {@code service} property.
+     *
+     * @param serviceBeanName the repository service bean name
+     */
+    public RepositoryFacade(String serviceBeanName) {
+        service = (IService) new ClassPathXmlApplicationContext("/META-INF/spring-data-context.xml")
+                .getBean(serviceBeanName);
+    }
+}
