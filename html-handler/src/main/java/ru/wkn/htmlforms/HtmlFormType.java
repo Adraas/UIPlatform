7e729508393dfab1bae838d1d6816990cca89eb0
Adraas <pickalov.artyom@yandex.ru>
2019-10-05 22:35:32 +0400

    feat: add HTML form type enumeration

---
+++ html-handler/src/main/java/ru/wkn/htmlforms/HtmlFormType.java
@@ -0,0 +1,13 @@
+package ru.wkn.htmlforms;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum HtmlFormType {
+
+    FORM("form");
+
+    private String htmlFormType;
+}
