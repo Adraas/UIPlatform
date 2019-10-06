9c71fb0d8e217b25490d82c94381e79756d9a943
Adraas <pickalov.artyom@yandex.ru>
2019-10-06 11:05:07 +0400

    feat: implement methods

--- html-handler/src/main/java/ru/wkn/htmlhandlers/generators/InputFormHtmlGenerator.java
+++ html-handler/src/main/java/ru/wkn/htmlhandlers/generators/InputFormHtmlGenerator.java
@@ -1,5 +1,6 @@
 package ru.wkn.htmlhandlers.generators;
 
+import org.jsoup.safety.Whitelist;
 import ru.wkn.RepositoryFacade;
 import ru.wkn.entities.HtmlTag;
 
@@ -7,13 +8,29 @@ import java.util.List;
 
 public class InputFormHtmlGenerator extends HtmlGenerator {
 
+    private Whitelist whitelist;
+
     public InputFormHtmlGenerator(RepositoryFacade repositoryFacade) {
         super(repositoryFacade);
+        initializeWhitelist();
+    }
+
+    private void initializeWhitelist() {
+        whitelist = new Whitelist()
+                .addTags("input", "select", "checkbox", "textarea")
+                .addAttributes("input", "accept", "align", "alt", "autocomplete",
+                        "autofocus", "border", "checked", "disabled", "form", "formaction", "formenctype", "formmethod",
+                        "formnovalidate", "formtarget", "list", "max", "maxlength", "min", "multiple", "name", "pattern",
+                        "placeholder", "readonly", "required", "size", "src", "step", "tabindex", "type", "value")
+                .addAttributes("select", "autofocus", "disabled", "form", "multiple", "name", "required",
+                        "size", "tabindex")
+                .addAttributes("textarea", "autofocus", "cols", "disabled", "form", "maxlength",
+                        "name", "placeholder", "readonly", "required", "rows", "tabindex", "wrap");
     }
 
     @Override
     public boolean htmlTagsFromStringIsValid(String htmlTags) {
-        return false;
+        return htmlTagsFromStringIsValid(htmlTags, whitelist);
     }
 
     @Override
