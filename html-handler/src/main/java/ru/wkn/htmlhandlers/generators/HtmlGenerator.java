06f119156ef364c93b12519d8867905aa7aeba20
Adraas <pickalov.artyom@yandex.ru>
2019-10-06 10:16:40 +0400

    feat: add attributes for the standard whitelist

--- html-handler/src/main/java/ru/wkn/htmlhandlers/generators/HtmlGenerator.java
+++ html-handler/src/main/java/ru/wkn/htmlhandlers/generators/HtmlGenerator.java
@@ -1,17 +1,32 @@
 package ru.wkn.htmlhandlers.generators;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import ru.wkn.RepositoryFacade;
 import ru.wkn.entities.HtmlTag;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@AllArgsConstructor
 @Getter
 public abstract class HtmlGenerator {
 
     private RepositoryFacade repositoryFacade;
+    private Map<String, String[]> standardWhitelistAttributesAndProtocols;
+
+    public HtmlGenerator(RepositoryFacade repositoryFacade) {
+        this.repositoryFacade = repositoryFacade;
+        standardWhitelistAttributesAndProtocols = new HashMap<>();
+        initializeStandardAttributesWithHTTProtocols();
+    }
+
+    private void initializeStandardAttributesWithHTTProtocols() {
+        String[] standardWhitelistAttributes = new String[]{"accesskey", "class", "contenteditable", "contextmenu",
+                "dir", "hidden", "id", "lang", "spellcheck", "style", "tabindex", "title", "xml:lang"};
+        for (String attribute : standardWhitelistAttributes) {
+            standardWhitelistAttributesAndProtocols.put(attribute, new String[]{"http", "https"});
+        }
+    }
 
     public abstract boolean htmlTagsFromStringIsValid(String htmlTags);
 
