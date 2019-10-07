bfb914dace4176b00cf16454fc12dff668ea50b3
Adraas <pickalov.artyom@yandex.ru>
2019-10-07 20:25:31 +0400

    docs: add documentation for the WebUIPlatform facade

--- html-handler/src/main/java/ru/wkn/WebUIPlatformFacade.java
+++ html-handler/src/main/java/ru/wkn/WebUIPlatformFacade.java
@@ -7,13 +7,32 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.wkn.htmlhandlers.generators.HtmlGenerator;
 import ru.wkn.htmlhandlers.wrappers.HtmlWrapper;
 
+/**
+ * The class {@code WebUIPlatformFacade} represents the facade pattern of the application core.
+ *
+ * @author Orin Adraas
+ */
 @Getter
 @Setter
 public class WebUIPlatformFacade {
 
+    /**
+     * The generator for the HTML pages.
+     */
     private HtmlGenerator htmlGenerator;
+
+    /**
+     * The wrapper for the HTML pages.
+     */
     private HtmlWrapper htmlWrapper;
 
+    /**
+     * Initializes a newly created {@code WebUIPlatformFacade} object with the assignment value from the context for the
+     * {@code services} property.
+     *
+     * @param htmlGeneratorName the generator for the HTML pages bean name
+     * @param htmlWrapperName   the wrapper for the HTML pages bean name
+     */
     public WebUIPlatformFacade(String htmlGeneratorName, String htmlWrapperName) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/META-INF/html-handler-context.xml");
         htmlGenerator = (HtmlGenerator) applicationContext.getBean(htmlGeneratorName);
