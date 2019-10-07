37737714601b7a7b0b65f3aa445b09c1f73894c4
Adraas <pickalov.artyom@yandex.ru>
2019-10-07 10:32:54 +0400

    docs: add documentation for the repositories The documentation for the JS and CSS repositories was added.

--- repository/src/main/java/ru/wkn/repository/JavaScriptFunctionRepository.java
+++ repository/src/main/java/ru/wkn/repository/JavaScriptFunctionRepository.java
@@ -3,7 +3,21 @@ package ru.wkn.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.wkn.entities.JavaScriptFunction;
 
+/**
+ * The interface {@code JavaScriptFunctionRepository} represents an abstract repository with the specific
+ * Spring Data JPA style. This interface provides customized functional for a work with a datasource of
+ * {@code JavaScriptFunctionRepository} type entities. It's the DAO layer of the repository.
+ *
+ * @author Orin Adraas
+ */
 public interface JavaScriptFunctionRepository extends CrudRepository<JavaScriptFunction, Integer> {
 
+    /**
+     * The method for the obtaining JavaScript function ({@code JavaScriptFunction} type) by the given function name as
+     * a {@code String} value.
+     *
+     * @param functionName the given JS function name as a {@code String} value for the searching
+     * @return a found {@code JavaScriptFunction} object
+     */
     JavaScriptFunction findJavaScriptFunctionByFunctionName(String functionName);
 }
--- repository/src/main/java/ru/wkn/repository/StylesheetRepository.java
+++ repository/src/main/java/ru/wkn/repository/StylesheetRepository.java
@@ -3,7 +3,20 @@ package ru.wkn.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.wkn.entities.Stylesheet;
 
+/**
+ * The interface {@code StylesheetRepository} represents an abstract repository with the specific Spring Data JPA style.
+ * This interface provides customized functional for a work with a datasource of {@code Stylesheet} type entities.
+ * It's the DAO layer of the repository.
+ *
+ * @author Orin Adraas
+ */
 public interface StylesheetRepository extends CrudRepository<Stylesheet, Integer> {
 
+    /**
+     * The method for the obtaining stylesheet ({@code Stylesheet} type) by the given selector as a {@code String} value.
+     *
+     * @param selector the given CSS selector as a {@code String} value for the searching
+     * @return a found {@code Stylesheet} object
+     */
     Stylesheet findStylesheetBySelector(String selector);
 }
