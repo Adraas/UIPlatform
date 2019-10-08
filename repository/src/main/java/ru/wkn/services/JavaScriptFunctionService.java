1b0f48f2d56f104860e4db85c1dea1938196f7e0
Adraas <pickalov.artyom@yandex.ru>
2019-10-08 12:08:25 +0400

    docs: add documentation for the services

--- repository/src/main/java/ru/wkn/services/JavaScriptFunctionService.java
+++ repository/src/main/java/ru/wkn/services/JavaScriptFunctionService.java
@@ -5,12 +5,28 @@ import org.springframework.stereotype.Service;
 import ru.wkn.entities.JavaScriptFunction;
 import ru.wkn.repository.JavaScriptFunctionRepository;
 
+/**
+ * The class {@code JavaScriptFunctionService} represents a repository services layer implementation.
+ *
+ * @author Orin Adraas
+ * @see IService
+ */
 @Service(value = "javaScriptFunctionService")
 @Getter
 public class JavaScriptFunctionService implements IService<JavaScriptFunction, Integer> {
 
+    /**
+     * The {@code repository} bean represented a repository (DAO) layer.
+     */
     private JavaScriptFunctionRepository repository;
 
+    /**
+     * The method for the obtaining JavaScript functions ({@code JavaScriptFunction} type) by the given function name as
+     * a {@code String} value.
+     *
+     * @param functionName the given JS function name as a {@code String} value for the searching
+     * @return a found collection of the {@code JavaScriptFunction} objects
+     */
     public Iterable<JavaScriptFunction> getJavaScriptFunctionsByFunctionName(String functionName) {
         return repository.findJavaScriptFunctionByFunctionName(functionName);
     }
--- repository/src/main/java/ru/wkn/services/StylesheetService.java
+++ repository/src/main/java/ru/wkn/services/StylesheetService.java
@@ -5,9 +5,18 @@ import org.springframework.stereotype.Service;
 import ru.wkn.entities.Stylesheet;
 import ru.wkn.repository.StylesheetRepository;
 
+/**
+ * The class {@code StylesheetService} represents a repository services layer implementation.
+ *
+ * @author Orin Adraas
+ * @see IService
+ */
 @Service(value = "stylesheetService")
 @Getter
 public class StylesheetService implements IService<Integer, Stylesheet> {
 
+    /**
+     * The {@code repository} bean represented a repository (DAO) layer.
+     */
     private StylesheetRepository repository;
 }
